9140ae2499736e7d52717456f62134c2cec243c5
denis farkas <denis.farkas@laplateforme.io>
2021-04-06 13:22:56 +0200

    cv

---
+++ views/main/moncv.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Titre du document</title>
+  </head>
+  <body>
+    <h1>Exemple PDF avec iframe</h1>
+    <iframe src="<?php echo WWW_ROOT; ?>public/Cv_denis-farkas.pdf" width="100%" height="500px">
+    </iframe>
+  </body>
+</html>
